93dc89674ed98296f30c5ef8058fe7adb8bcec87
Fernando Nonato <105546921+Cyberfn@users.noreply.github.com>
2023-03-01 20:12:21 -0300

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,24 @@
+package Questoes;
+
+import java.util.Scanner;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+Scanner entrada = new Scanner (System.in);
+		
+		System.out.println("Digite o a temperatura em Celsius:");
+		 double C = entrada.nextDouble();
+		 
+		 double F = (C * 1.8) + 32;
+		 
+		 System.out.println("A temperatura em Farenheit é " + F + "°");
+		
+		
+		
+
+	}
+
+}
